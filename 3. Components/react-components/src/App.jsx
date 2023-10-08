dc374c2b985580c29b6eae2660511cb3b880ef91
facupaez <facu.pz10@gmail.com>
2023-10-08 12:26:01 -0300

    class and functional components

---
+++ 3. Components/react-components/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Componente from "./components/Componente";
+
+function App() {
+  return (
+    <>
+      {/* <Componente msg="Hola soy un componente desde props" /> */}
+      {/* <Componente msg="Hola soy un componente funcional desde props" /> */}
+      <Componente msg="Hola soy un componente funcional expresado desde props" />
+    </>
+  );
+}
+
+export default App;
