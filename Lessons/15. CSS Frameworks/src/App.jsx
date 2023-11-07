a034277632be0a70aedfb03b2601ee43dd6e7d5a
facupaez <facu.pz10@gmail.com>
2023-11-06 21:06:53 -0300

    Bootstrap and Bulma frameworks

---
+++ Lessons/15. CSS Frameworks/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Bootstrap from "./components/Bootstrap";
+import Bulma from "./components/Bulma";
+function App() {
+  return (
+    <>
+      <h1>Frameworks CSS con React</h1>
+      {/*       <Bootstrap /> z */}
+      <Bulma />
+    </>
+  );
+}
+
+export default App;
