5b4a32a20ff44c92b07fdc4f9e3b0429219e844c
facupaez <facu.pz10@gmail.com>
2023-11-15 22:13:13 -0300

    CRUD Api: building an api with json-server

---
+++ Lessons/17. CRUD Api/src/components/CrudApi.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+
+const CrudApi = () => {
+  const [db, setDb] = useState([]);
+  const [dataToEdit, setDataToEdit] = useState(null);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    /*  console.log(data); */
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = (id) => {
+    let isDeleted = confirm(`Estás seguro de eliminar el caballero ${id}`);
+
+    if (isDeleted) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Crud Api</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        <CrudTable
+          data={db}
+          setDataToEdit={setDataToEdit}
+          deleteData={deleteData}
+        />
+      </article>
+    </div>
+  );
+};
+
+export default CrudApi;
