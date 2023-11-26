015e9a5a436bc8c454ebe2871f74f6b1d8ddeb9a
facupaez <facu.pz10@gmail.com>
2023-11-26 09:21:20 -0300

    SongSearch: AJAX requests

--- Lessons/18. Songs search/src/components/SongSearch.jsx
+++ Lessons/18. Songs search/src/components/SongSearch.jsx
@@ -2,12 +2,40 @@ import React, { useState, useEffect } from "react";
 import SongForm from "./SongForm";
 import SongDetails from "./SongDetails";
 import SongLoader from "./SongLoader";
+import { helpHttp } from "../helpers/helpHttp";
+
+// LAS APIS EXTERNAS DE ESTE PROYECTO NO FUNCIONAN
 
 const SongSearch = () => {
   const [search, setSearch] = useState(null);
   const [lyric, setLyric] = useState(null);
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+
+      let artistUrl = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      setLoading(true);
+
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(songUrl),
+      ]);
+
+      setBio(artistRes);
+      setLyric(songRes);
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [search]);
 
   const handleSearch = (data) => {
     console.log(data);
