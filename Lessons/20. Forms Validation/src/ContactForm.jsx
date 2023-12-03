70d66ead3641943c08a7caf1ee80bb4a3c1b553d
facupaez <facu.pz10@gmail.com>
2023-12-02 21:01:47 -0300

    Forms Validation: validations development

--- Lessons/20. Forms Validation/src/ContactForm.jsx
+++ Lessons/20. Forms Validation/src/ContactForm.jsx
@@ -10,9 +10,31 @@ const initialForm = {
 
 const validationsForm = (form) => {
   let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,144}$/;
 
   if (!form.name.trim()) {
     errors.name = "El campo 'Nombre' es requerido";
+  } else if (!regexName.test(form.name.trim())) {
+    errors.name = "El campo 'Nombre' solo acepta letras y espacios en banco";
+  }
+
+  if (!form.email.trim()) {
+    errors.email = "El campo 'Email' es requerido";
+  } else if (!regexEmail.test(form.email.trim())) {
+    errors.email = "El campo 'Email' debe ser un email válido";
+  }
+
+  if (!form.subject.trim()) {
+    errors.subject = "El campo 'Asunto a tratar' es requerido";
+  }
+
+  if (!form.comments.trim()) {
+    errors.comments = "El campo 'Comentarios' es requerido";
+  } else if (!regexComments.test(form.comments.trim())) {
+    errors.comments =
+      "El campo 'Comentarios' debe tener menos de 144 carácteres";
   }
 
   return errors;
