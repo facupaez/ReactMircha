3dac929e0c6b56055a509b43d29243c342355da5
facupaez <facu.pz10@gmail.com>
2023-12-03 10:15:48 -0300

    Modals Windows: modal portals

--- Lessons/21. Modals Windows/src/components/Modals.jsx
+++ Lessons/21. Modals Windows/src/components/Modals.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Modal from "./Modal";
 import { useModal } from "./hooks/useModal";
+import ModalPortal from "./ModalPortal";
 
 const Modals = () => {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenPortal, openPortal, closePortal] = useModal(false);
   return (
     <div>
       <h2>Ventanas Modales</h2>
@@ -18,6 +20,14 @@ const Modals = () => {
         <h3>Modal 2</h3>
         <p>Hola, este es el contenido del modal 2</p>
       </Modal>
+      <button onClick={openPortal}>Modal Portal</button>
+      <ModalPortal isOpen={isOpenPortal} closeModal={closePortal}>
+        <h3>Modal Portal</h3>
+        <p>
+          Hola, este es el contenido de un modal Portal, que carga en otro nodo
+          diferente del DOM diferente a donde carga nuestra app de React.
+        </p>
+      </ModalPortal>
     </div>
   );
 };
