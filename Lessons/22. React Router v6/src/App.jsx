abf711a802ff6edd47f5687d0980df932f9d50b5
facupaez <facu.pz10@gmail.com>
2023-12-03 11:29:55 -0300

    React Router 6: pages, header and menu restructure

--- Lessons/22. React Router v6/src/App.jsx
+++ Lessons/22. React Router v6/src/App.jsx
@@ -1,43 +1,24 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
 import "./App.css";
-
-function Acerca() {
-  return <h1>Acerca</h1>;
-}
+import { Header } from "./components/Header";
+import { Menu } from "./components/Menu";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+    <div className="App">
       <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/acerca">Acerca</Link>
-        </nav>
+        <Header />
+        <Menu />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/acerca" element={<Acerca />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/acerca" element={<About />} />
+          <Route path="/contacto" element={<Contact />} />
         </Routes>
       </Router>
-    </>
+    </div>
   );
 }
 
---
+++ Lessons/22. React Router v6/src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export function Menu() {
+  return (
+    <nav className="menu">
+      <Link to="/">Home</Link>
+      <Link to="/acerca">Acerca</Link>
+      <Link to="/contacto">Contacto</Link>
+    </nav>
+  );
+}
