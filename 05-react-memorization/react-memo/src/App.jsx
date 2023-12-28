e606a9b77e8eb5644084c3e771951e23e940d879
facupaez <facu.pz10@gmail.com>
2023-12-27 22:32:59 -0300

    Memorization: useMemo

--- 05-react-memorization/react-memo/src/App.jsx
+++ 05-react-memorization/react-memo/src/App.jsx
@@ -14,28 +14,28 @@ function App() {
         >
           Memo
         </a>
-        <ul>
-          <li>Se encarga de memorizar un componente,</li>
-          <li>
-            Lo vuelve a memorizar al momento de que sus <b>props</b> cambian.
-          </li>
-          <li>Evita re-renderizados.</li>
-          <li>
-            Hay que evitarlo en la medida de lo posible, pues podría ser más
-            costosa la tarea de memorización que el re-renderizado del
-            componente.
-          </li>
-          <li>
-            Úsalo, cuando:
-            <ul>
-              <li>Tenemos muchos elementos renderizados en una lista.</li>
-              <li>Llamamos datos de APIs.</li>
-              <li>Un componente se vuelve muy pesado.</li>
-              <li>Salen alertas de rendimiento en la consola.</li>
-            </ul>
-          </li>
-        </ul>
       </h3>
+      <ul>
+        <li>Se encarga de memorizar un componente,</li>
+        <li>
+          Lo vuelve a memorizar al momento de que sus <b>props</b> cambian.
+        </li>
+        <li>Evita re-renderizados.</li>
+        <li>
+          Hay que evitarlo en la medida de lo posible, pues podría ser más
+          costosa la tarea de memorización que el re-renderizado del componente.
+        </li>
+        <li>
+          Úsalo, cuando:
+          <ul>
+            <li>Tenemos muchos elementos renderizados en una lista.</li>
+            <li>Llamamos datos de APIs.</li>
+            <li>Un componente se vuelve muy pesado.</li>
+            <li>Salen alertas de rendimiento en la consola.</li>
+          </ul>
+        </li>
+      </ul>
+
       <hr />
       <h3>
         <a
@@ -59,7 +59,22 @@ function App() {
         </li>
       </ul>
       <hr />
-
+      <h3>
+        <a
+          href="https://es.reactjs.org/docs/hooks-reference.html#usememo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          useMemo
+        </a>
+      </h3>
+      <ul>
+        <li>
+          Memoriza un valor calculado, es decir, el resultado de una función.
+        </li>
+        <li>Genera propiedades computadas.</li>
+        <li>Úsalo en procesos pesados.</li>
+      </ul>
       <hr />
       <Contador />
     </div>
