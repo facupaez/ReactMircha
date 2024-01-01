448fb6cfa432036af0b10702206a81ac6e595abf
facupaez <facu.pz10@gmail.com>
2024-01-01 12:40:10 -0300

    Reducers: introduction with count example

---
+++ 07-react-reducers/00-reducers/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Contador from "./components/Contador";
+
+function App() {
+  return (
+    <>
+      <h1>Reducers</h1>
+      <hr />
+      <Contador />
+    </>
+  );
+}
+
+export default App;
